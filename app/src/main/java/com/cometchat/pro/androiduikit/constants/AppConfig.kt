7082984ffb937388f5e1aff6c2a0a109afd20df1
Vilgheff <vn29082000@gmail.com>
2022-06-17 15:09:25 +0700

    commit_appconfig

--- app/src/main/java/com/cometchat/pro/androiduikit/constants/AppConfig.kt
+++ app/src/main/java/com/cometchat/pro/androiduikit/constants/AppConfig.kt
@@ -2,8 +2,8 @@ package com.cometchat.pro.androiduikit.constants
 
 class AppConfig {
     object AppDetails {
-        const val APP_ID = "209344bbda43bc7d"
-        const val AUTH_KEY = "5e2fbc96565231ff800668e8e4832462e8c94347"
+        const val APP_ID = "21217691a17d7b3a"
+        const val AUTH_KEY = "2151d7b7d689d269e4db7521b1a8f0a00ac1e1bf"
         const val REGION = "us"
         const val FIREBASE_REALTIME_DATABASE_URL = "https://chatapp-b0a14-default-rtdb.firebaseio.com/"
         const val FIREBASE_EMAIL_KEY = "email" // this is not a secret value, it is just a constant variable that will be accessed from different places of the application.
